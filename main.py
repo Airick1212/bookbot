0c3002732c2c06e40211782329d0b0b714036cdc
Airick1212 <eric@ericanthonydavis.com>
2025-11-28 10:58:26 -0800

    update main and stats for working changes

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
-def num_words(bookText):
-    words = bookText.split()
-    return len(words)
+from stats import get_num_words
 
 def main():
     bookText = get_book_text("./books/frankenstein.txt")
-    print(f"Found {num_words(bookText)} total words.")
+    print(f"Found {get_num_words(bookText)} total words.")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,3 @@
-def num_words(bookText):
+def get_num_words(bookText):
     words = bookText.split()
     return len(words)

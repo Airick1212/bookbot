dabd72141d49bcbd6614ac8612551cca98b02e83
Airick1212 <eric@ericanthonydavis.com>
2025-11-29 21:22:03 -0800

    yes

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
-from stats import get_num_words
+from stats import get_num_words, get_num_chars
 
 def main():
     bookText = get_book_text("./books/frankenstein.txt")
     print(f"Found {get_num_words(bookText)} total words.")
+    
+    list = get_num_chars(bookText)
+
+    for key, value in list.items():
+        print("'" + key + "'" + ": " + str(value))
 
 if __name__ == "__main__":
     main()
+
+print(get_num_chars(get_book_text("./books/frankenstein.txt")))
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_num_words(bookText):
     words = bookText.split()
     return len(words)
+
+def get_num_chars(bookText):
+    num_cars_dict = {}
+    for char in bookText:
+        if char == " ":
+            continue
+        if char.lower() in num_cars_dict:
+            num_cars_dict[char.lower()] += 1
+        else:
+            num_cars_dict[char.lower()] = 1
+    return num_cars_dict
